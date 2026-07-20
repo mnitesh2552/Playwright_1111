bceebc0450960743845ccccbb70b1eaeb4beb635
Nitesh Kumar <mnitesh.0055@gmail.com>
2026-07-20 16:57:14 +0530

    Added multiple element examples

--- tests/06_Multiple-Element_/20_tta_student_login.spec.ts
+++ tests/06_Multiple-Element_/20_tta_student_login.spec.ts
@@ -1,63 +1,68 @@
-//Verify the URL is changed with your username and password
+// Verify the URL is changed with your username and password
+
 import { test, expect } from '@playwright/test';
 
 test('Verify URL changes with username and password', async ({ page }) => {
-    await page.goto('https://https://app.thetestingacademy.com/playwright/multiple_element_filter');
-    const inputs = page.locator("//input");
-    const total = await inputs.count();
 
-    for (let i = 0; i < total; i++) {
+    await page.goto('https://app.thetestingacademy.com/playwright/multiple_element_filter');
+
+    // Collect all input elements
+    const inputs = page.locator("//input");
+    const totalInputs = await inputs.count();
+
+    // Fill Email
+    for (let i = 0; i < totalInputs; i++) {
 
         const id = await inputs.nth(i).getAttribute("id");
 
         if (id === "email") {
-
             await inputs.nth(i).fill("nitesh@thetestingacademy.com");
             break;
         }
     }
-    const inputs = page.locator("//input");
 
-    const total = await inputs.count();
-
-    for (let i = 0; i < total; i++) {
+    // Fill Password
+    for (let i = 0; i < totalInputs; i++) {
 
         const id = await inputs.nth(i).getAttribute("id");
 
         if (id === "password") {
-
             await inputs.nth(i).fill("admin123");
             break;
         }
     }
-    const inputs = page.locator("//input");
 
-    const total = await inputs.count();
-
-    for (let i = 0; i < total; i++) {
+    // Check Remember Me
+    for (let i = 0; i < totalInputs; i++) {
 
         const type = await inputs.nth(i).getAttribute("type");
 
         if (type === "checkbox") {
-
             await inputs.nth(i).check();
             break;
         }
     }
+
+    // Collect all buttons
     const buttons = page.locator("//button");
+    const totalButtons = await buttons.count();
 
-    const total = await buttons.count();
-
-    for (let i = 0; i < total; i++) {
+    // Click Login Button
+    for (let i = 0; i < totalButtons; i++) {
 
         const text = await buttons.nth(i).innerText();
 
         if (text === "Login to Practice Account") {
-
             await buttons.nth(i).click();
             break;
         }
     }
+
+    // Verify URL
     const expectedURL = "https://app.thetestingacademy.com/playwright/multiple_element_filter?email=nitesh%40thetestingacademy.com&password=admin123&remember=yes#login-success";
 
     await expect(page).toHaveURL(expectedURL);
+
+    await page.pause();
+
+});
